39a9791313183919689fed0b51134f891fc911df
HENILCODES <prajapatihenil1112@gmail.com>
2022-10-14 21:28:05 +0530

    e

--- src/App.js
+++ src/App.js
@@ -4,18 +4,24 @@ export default class App extends Component {
   constructor() {
     super();
     console.log("Constr");
+    this.state= {
+      data: this.data
+    } 
   }
   async componentDidMount() {
     console.log("Start");
+
     let ob = await fetch("https://henil-code.github.io/test/sem4Result.json");
     let data = await ob.json();
     console.log(data);
+    return data
   }
+
   render() {
     return (
-      // <>{console.log(this.state.d)}
+      <>{console.log(this.state.data)}
         <h1>Hello</h1>
-      // </>
+      </>
     );
   }
 }
