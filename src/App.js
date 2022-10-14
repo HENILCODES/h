0dd85922c2bc3d421c4b8397bcbb37216462891d
HENILCODES <prajapatihenil1112@gmail.com>
2022-10-14 20:56:45 +0530

    a

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import React, { Component } from "react";
 export default class App extends Component {
   constructor() {
     super();
-    this.d= this.data;
+    console.log("Constr");
   }
   async componentDidMount() {
-    let ob = await fetch("https://henil-code.github.io/test/data.json");
+    console.log("Start");
+    let ob = await fetch("https://henil-code.github.io/test/sem4Result.json");
     let data = await ob.json();
-    console.log(data[0].gender);
+    console.log(data);
   }
   render() {
     return (
